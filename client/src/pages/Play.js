54f179a75eccc890997c3affbd8fcc96f809674b
Lucy Kendall <lucykendall88@gmail.com>
2021-12-18 12:41:49 +1100

    stylings

--- client/src/pages/Play.js
+++ client/src/pages/Play.js
@@ -7,7 +7,7 @@ export default function Play() {
     return (
         <Container className="d-flex align-items-center justify-content-center" style={{ maxWidth: "900px"}}>
             <div className="w-100" style={{ maxHeight: "900px", margin: "20px auto" }}>
-                <h1 className="h2" style={{ textAlign: "center", color: "rgb(37, 86, 54)"}}>Memory Game!</h1>
+                <h1 className="h2" style={{ textAlign: "center", color: "rgb(37, 86, 54)"}}>Click New Game to begin! </h1>
                 <Board></Board>
             </div>
         </Container>
